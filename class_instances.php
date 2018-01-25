032e776b5017ed3cc1a29142f083e880fea67efd
cooleydw494 <cooleydw494@gmail.com>
2018-01-25 09:30:51 -0500

    first few examples of OOP PHP course on lynda (kevin skoglund)

---
+++ class_instances.php
@@ -0,0 +1,21 @@
+<?php
+
+class Student {
+
+}
+
+$student1 = new Student;
+$student2 = new Student;
+
+echo get_class($student1) . '<br />';
+
+$class_names = ['Product', 'Student', 'student'];
+foreach ($class_names as $class_name) {
+  if (is_a($student1, $class_name)) {
+    echo "student1 is an instance of {$class_name}.<br />";
+  } else {
+    echo "student1 is not an instance of {$class_name}.<br />";
+  }
+}
+
+ ?>
